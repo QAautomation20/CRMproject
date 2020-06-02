b7c04c23cf8bd4a6e932e7126fedb90b6bd66c9a
Niraj <n_shre@yahoo.com>
2020-06-02 00:58:09 -0500

    almost done

--- src/main/java/page/CRMDealPage.java
+++ src/main/java/page/CRMDealPage.java
@@ -1,10 +1,13 @@
 package page;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CRMDealPage {
 
@@ -21,35 +24,40 @@ public class CRMDealPage {
 	 WebElement Title;
 	 @FindBy(how =How.XPATH, using ="//div[@class='ui fluid selection dropdown'] //i[@class='dropdown icon']")
 	 WebElement AssignedTo;
-	 @FindBy(how =How.XPATH, using ="//div[@name='company'] //div[@class='text']")
+	 @FindBy(how =How.XPATH, using ="//div[@class='visible menu transition']")
+	 WebElement QualityAutomation;
+	 @FindBy(how =How.XPATH, using ="//div[@name='company']/input[@class='search']")
 	 WebElement Company;
-	 @FindBy(how =How.XPATH, using ="//span[contains(text(),'Add')]")
+	 @FindBy(how =How.XPATH, using ="//div[@name='company']/descendant::div[@class='visible menu transition']")
 	 WebElement AddCompany;
-	 @FindBy(how =How.NAME, using ="products")
-	 WebElement Products;
-	 @FindBy(how =How.XPATH, using ="//span[contains(text(),'Add')]")
-	 WebElement AddProducts;
-	 @FindBy(how =How.XPATH, using ="//div[@name='contacts']")
+	 @FindBy(how =How.XPATH, using ="//div[@name='contacts']//input[@class='search']")
 	 WebElement Contacts;
-	 @FindBy(how =How.XPATH, using ="//div[@name='contacts'] //span[contains(text(),'Naya')]")
-	 WebElement AddContacts;
-	 @FindBy(how =How.XPATH, using ="//div[@class='react-datepicker']")
-	 WebElement DatePicker;
+	 @FindBy(how =How.XPATH, using ="(//span[contains(text(),'Baigaan')])[3]")
+	 WebElement SelectContacts;
 
+	 @FindBy(how =How.XPATH, using ="//div[@class='react-datepicker__input-container']/input")
+	 WebElement DatePicker;
+	 @FindBy(how =How.XPATH, using ="//div[@class='react-datepicker__header']/div")
+	 WebElement Month;
+	 @FindBy(how =How.XPATH, using ="//div[@class='react-datepicker-popper'] //div[@class='react-datepicker__day react-datepicker__day--026']")
+	 WebElement SelectDate;
+	 @FindBy(how =How.XPATH, using ="//div[@class='react-datepicker__time-container '] //li[@class='react-datepicker__time-list-item '][5]")
+	 WebElement SelectTime;
 	public void createNewDeal() throws InterruptedException {
 		Deals.click();
 		Title.sendKeys("Software products");
 		AssignedTo.click();
 		Thread.sleep(3000);
-
+		QualityAutomation.click();
 		Company.sendKeys("Niraj");
 		AddCompany.click();
-		Products.sendKeys("CRM platform");
-		AddProducts.click();
 		Contacts.click();
-		AddContacts.click();
-		Select datePicker = new Select(DatePicker);
-		datePicker.selectByVisibleText("Next Month");
+		Contacts.sendKeys("B");
+		new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(SelectContacts));
+		SelectContacts.click();
+		DatePicker.click();
+		SelectTime.click();
+		//Month.sendKeys(Keys.ENTER);
 		
 	}	
 }
--- src/test/java/test/CRMTest.java
+++ src/test/java/test/CRMTest.java
@@ -13,7 +13,7 @@ public class CRMTest extends CRMBasePage {
 
 	WebDriver driver;	
 
-	@Test(priority = 1)
+	//@Test(priority = 1)
 	public void createQuickDraft() throws InterruptedException {
 		driver = CRMBrowserFactory.startBrowser();
 		CRMLoginPage loginPage = PageFactory.initElements(driver, CRMLoginPage.class);
